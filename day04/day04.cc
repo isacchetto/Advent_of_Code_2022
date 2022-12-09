e792766cbad03688f91cd2e276b4af92c0b6e8a9
isacchetto <isacco.cenacchi@gmail.com>
2022-12-09 20:21:21 +0100

    4.2

--- day04/day04.cc
+++ day04/day04.cc
@@ -2,11 +2,11 @@
 using namespace std;
 
 void conversion(string buffer,int& min1,int& max1,int& min2,int& max2) {
-
-    min1=stoi(buffer.substr(0, buffer.find("-")));
-    max1=stoi(buffer.substr(buffer.find("-")+1,buffer.find(",")));
-    min2=stoi(buffer.substr(buffer.find(",")+1,buffer.find_last_of("-")));
-    max2=stoi(buffer.substr(buffer.find_last_of("-")+1,string::npos));
+    // create a substring that start from 1argoument (included) to 2argoument (not included)
+    min1=stoi(buffer.substr(0, buffer.find("-")));                          // .find() return the pos of the first occ. of "-"
+    max1=stoi(buffer.substr(buffer.find("-")+1,buffer.find(",")));          // +1 for non include "-"
+    min2=stoi(buffer.substr(buffer.find(",")+1,buffer.find_last_of("-")));  // .find_last_of() return the pos of the last occ. of one of this char "-"
+    max2=stoi(buffer.substr(buffer.find_last_of("-")+1,string::npos));      // string::npos = -1 , in unsigned int, "until the end of the string"
 
 }
 
@@ -16,15 +16,16 @@ int puzzle1() {
     int min1, max1;
     int min2, max2;
     int result=0;
-    while(cin>>buffer) {
-        
-        // works only for one digit numbers
-        //min1=stoi(string(1,buffer[0]));       //create a substring of one char and use stoi()
-        //max1=stoi(buffer.substr(2,3));        //create a substring from 2 to 3 position
-        //min2=atoi(&buffer[4]);                //take the address of buffer[4] because atoi require a pointer: char* c=&buffer[4]
-        //max2=atoi(buffer.c_str()+6));         //use .c_str() or .data() to return a char* 
 
-        conversion(buffer,min1,max1,min2,max2); //min e max passati per riferimento
+    while(cin>>buffer) {
+
+        // works only for one digit numbers
+        //min1=stoi(string(1,buffer[0]));       // create a substring of one char and use stoi()
+        //max1=stoi(buffer.substr(2,3));        // create a substring from 2 to 3 position
+        //min2=atoi(&buffer[4]);                // take the address of buffer[4] because atoi require a pointer: char* c=&buffer[4]
+        //max2=atoi(buffer.c_str()+6));         // use .c_str() or .data() to return a char* 
+
+        conversion(buffer,min1,max1,min2,max2); // min e max passed by reference
 
 
         if(min1<=min2 && max1>=max2) {          // if 1 contain or equal to 2
@@ -39,13 +40,26 @@ int puzzle1() {
 }
 
 int puzzle2() {
+    ifstream cin("input.txt");
+    string buffer;
+    int min1, max1;
+    int min2, max2;
+    int result=0;
 
-    return 0;
+    while(cin>>buffer) {
+        conversion(buffer,min1,max1,min2,max2);
+        if(min1>=min2 && min1<=max2 || min2>=min1 && min2<=max1) {  //min1 between 2 || min2 between 1
+            result++;
+        
+        }
+    }
+
+    return result;
 }
 
 int main() {
-    cout<<puzzle1()<<endl;
-    //cout<<puzzle2()<<endl;
+    //cout<<puzzle1()<<endl;
+    cout<<puzzle2()<<endl;
     
     return 0;
 }
